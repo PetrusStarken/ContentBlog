c4f95e5c83683201b0156c6ea8568e36bcdd400d
PetrusStarken <pedrothm91@gmail.com>
2017-09-17 09:04:41 -0300

    webapi configured to enable CORS

--- WebApi/WebApi/App_Start/WebApiConfig.cs
+++ WebApi/WebApi/App_Start/WebApiConfig.cs
@@ -1,14 +1,17 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web.Http;
-
-namespace WebApi
+﻿namespace WebApi
 {
+    using System.Configuration;
+    using System.Web.Http;
+    using System.Web.Http.Cors;
+
     public static class WebApiConfig
     {
         public static void Register(HttpConfiguration config)
         {
+            // Enabling CORS for our webapp
+            var webAppAddress = ConfigurationManager.AppSettings["webAppAddress"].ToString();
+            var corsAttr = new EnableCorsAttribute(webAppAddress, "*", "*");
+            config.EnableCors(corsAttr);
             // Web API configuration and services
 
             // Web API routes
